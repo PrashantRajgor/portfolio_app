dbb5bbbb5e601157e908677e06d41712b7af4c7d
unknown <Administrator@celab2-5.comp1-domain.spit.ac.in>
2012-09-17 13:06:04 +0530

    all is clear

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,13 +1,14 @@
   class Company < ActiveRecord::Base
     validate :symbol_four_or_five_letters
     def symbol_four_or_five_letters
-      if (symbol.length<4 or symbol.length>5 )
-       errors.add :symbol, 'not more thane four to five letters are allowed'
+      if  symbol.length>4
+       errors.add :symbol,'not more thane four to five letters are allowed'
       end
     end
+
+ validate :cost_should_be_positive
+def cost_should_be_positive
+   if (price<=0)
+     errors.add :price,'should be positive and contain some values'
   end
- # validate :cost_should_be_positive
-  #def cost_should_be_positive
-   # if (cost<0)
-    #  errors.add :cost,'should be positive'
-    #end
+  end  end
