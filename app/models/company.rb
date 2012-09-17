7c652375ea0fdf04e43843e44eab1a98f5a4ae6e
unknown <Administrator@celab2-5.comp1-domain.spit.ac.in>
2012-09-17 12:49:31 +0530

    evrything except error in symbol

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,13 @@
-class Company < ActiveRecord::Base
-end
+  class Company < ActiveRecord::Base
+    validate :symbol_four_or_five_letters
+    def symbol_four_or_five_letters
+      if (symbol.length<4 or symbol.length>5 )
+       errors.add :symbol, 'not more thane four to five letters are allowed'
+      end
+    end
+  end
+ # validate :cost_should_be_positive
+  #def cost_should_be_positive
+   # if (cost<0)
+    #  errors.add :cost,'should be positive'
+    #end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,16 @@
 class Investment < ActiveRecord::Base
+validate :quantity_should_be_positive
+def quantity_should_be_positive
+if (qty<0)
+  errors.add :qty,'should be positive'
+end
+end
+validate :cost_should_be_positive
+def cost_should_be_positive
+  if (cost<0)
+    errors.add :cost,'should be positive'
+end
+
+
+end
 end
